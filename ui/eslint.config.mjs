70c204ac574d2980526be49a9e9f430ba01f1bd2
Pandu Wijaya <157868393+coderhana28@users.noreply.github.com>
2025-06-13 16:02:00 +0700

    install shadcn, add basic component, clean up

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -49,34 +49,3 @@ export default [
         },
     },
 ];
-
-// import next from 'eslint-config-next';
-// import tseslint from '@typescript-eslint/eslint-plugin';
-// import tsparser from '@typescript-eslint/parser';
-// import prettierPlugin from 'eslint-plugin-prettier';
-
-// const esLintConfig = [
-// 	next(),
-// 	{
-// 		files: ['**/*.ts', '**/*.tsx'],
-// 		languageOptions: {
-// 			parser: tsparser,
-// 			parserOptions: {
-// 				project: './tsconfig.json',
-// 			},
-// 		},
-// 		plugins: {
-// 			'@typescript-eslint': tseslint,
-// 			prettier: prettierPlugin,
-// 		},
-// 		rules: {
-// 			// Example TypeScript rule overrides (optional)
-// 			'@typescript-eslint/no-unused-vars': 'warn',
-
-// 			// Prettier as ESLint rule
-// 			'prettier/prettier': 'warn',
-// 		},
-// 	},
-// ];
-
-// export default esLintConfig;
